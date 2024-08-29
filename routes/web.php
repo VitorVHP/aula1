caad4263a6a963505663e829a708137dcc8989cf
VitorVHP <vitor.ramos01@fatec.sp.gov.br>
2024-08-29 10:25:52 -0300

    29/08 - Criação Laravel, Site fatec, NomeProfissão

---
+++ routes/web.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/tela', function () {
+    return view('minha-tela');
+});
+
+Route::get('/nome/{nome}/{profissao}', function (string $nome, string $profissao) {
+    return "Olá $nome, você é o melhor $profissao do mundo!";
+});
